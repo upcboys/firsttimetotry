934966d0664d9bd703563487ac27056da569cf42
AStone <601780480@qq.com>
2016-10-02 15:37:51 +0800

    add
    add2

--- main.java
+++ main.java
@@ -5,6 +5,9 @@ import Triangle.WindowTriangle;
 /**
  * Created by shiyu on 16/8/22.
  */
+/*
+ 这是一个测试版本
+ */
 public class main {
     public static void main(String [] args){
         WindowColor win = new WindowColor();
